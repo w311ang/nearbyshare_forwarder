5c600d51e0c092b4182a1f2606e3c420d43d97f8
w311ang <w311angw311ang@gmail.com>
2024-02-20 22:32:44 +0800

    commit

---
+++ app/src/main/java/com/w311ang/nearbyshare_forwarder/ShareActivity.java
@@ -0,0 +1,31 @@
+package com.w311ang.nearbyshare_forwarder;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Toast;
+import android.content.ActivityNotFoundException;
+
+public class ShareActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+		//Toast.makeText(getApplication(), "hi.", Toast.LENGTH_SHORT).show();
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_share);
+
+        // 获取原始的分享Intent
+        Intent originalIntent = new Intent(getIntent());
+
+        // 修改Intent的action和package，以便启动Nearby Share
+        originalIntent.setAction("com.google.android.gms.SHARE_NEARBY");
+        originalIntent.setPackage("com.google.android.gms");
+		originalIntent.setComponent(null);
+
+        // 尝试启动Nearby Share
+        try {
+            startActivity(originalIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "无法找到Nearby Share来分享内容", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
